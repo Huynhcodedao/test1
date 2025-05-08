faa8ced4fc17e48a5e69a918eb957a0698e5ed8a
Huynhcodedao <huynhpro1709@gmail.com>
2025-05-08 12:54:59 +0700

    Cập nhật mã nguồn (train.py, dataset.py, generate.py, config.py) và thêm dataset data/

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,8 +1,9 @@
 import os
 import torch
+import wandb
 import numpy as np
 from utils.data_augment import WiderFacePreprocess
-from model.config import INPUT_SIZE
+from model.config import INPUT_SIZE, TRAIN_PATH, VALID_PATH
 from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
@@ -17,84 +18,63 @@ class WiderFaceDataset(Dataset):
                 mentioned in the paper. Only applied on the train split.
     """
 
-    def __init__(self, root_path, input_size=INPUT_SIZE, is_train=True, label_file='labels.txt'):
-        self.ids = []
-        self.label_dict = {}
+    def __init__(self, root_path, input_size=INPUT_SIZE, is_train=True):
+        self.ids       = []
         self.transform = WiderFacePreprocess(image_size=input_size)
-        self.is_train = is_train
+        self.is_train  = is_train
 
-        # Đặt đường dẫn đến thư mục hình ảnh và nhãn
-        self.image_path = os.path.join(root_path, 'image')
-        self.label_path = os.path.join(root_path, label_file)
-
-        # Đọc labels.txt
-        print(f"Đang cố mở tệp: {self.label_path}")
-        with open(self.label_path, 'r') as f:
-            lines = f.readlines()
-        i = 0
-        while i < len(lines):
-            img_name = lines[i].strip()
-            num_bbox = int(lines[i+1].strip())
-            bboxes = []
-            for j in range(num_bbox):
-                bboxes.append(lines[i+2+j].strip().split())
-            base = os.path.splitext(os.path.basename(img_name))[0]
-            self.label_dict[base] = bboxes
-            i = i + 2 + num_bbox
-
-        # Lấy danh sách hình ảnh
-        all_images = [f[:-4] for f in os.listdir(self.image_path) if f.endswith('.png')]
+        if is_train: 
+            self.path = os.path.join(root_path, TRAIN_PATH)
+        else: 
+            self.path = os.path.join(root_path, VALID_PATH)
         
-        # Chia train/val (80% train, 20% val)
-        split_idx = int(0.8 * len(all_images))
-        if is_train:
-            self.ids = all_images[:split_idx]
-        else:
-            self.ids = all_images[split_idx:]
-
-        print(f"Số lượng mẫu {'train' if is_train else 'val'}: {len(self.ids)}")
+        for dirname in os.listdir(os.path.join(self.path, 'images')):
+            for file in os.listdir(os.path.join(self.path, 'images', dirname)):
+                self.ids.append(os.path.join(dirname, file)[:-4])
 
     def __len__(self):
         return len(self.ids)
 
     def __getitem__(self, index):
-        img_name = self.ids[index]
-        img_path = os.path.join(self.image_path, f"{img_name}.png")
-        img = Image.open(img_path)
+        img = Image.open(os.path.join(self.path, 'images', self.ids[index]+'.jpg'))
         img = np.array(img)
 
-        # Lấy nhãn từ label_dict
-        bboxes = self.label_dict.get(img_name, [])
-        annotations = np.zeros((len(bboxes), 15))
+        f = open(os.path.join(self.path, 'labels', self.ids[index]+'.txt'), 'r')
+        lines = f.readlines()
 
-        if len(bboxes) == 0:
-            return img, annotations
+        annotations = np.zeros((len(lines), 15)) 
 
-        for idx, bbox in enumerate(bboxes):
-            bbox = [float(x) for x in bbox]
+        if len(lines) == 0:
+            return annotations
+        
+        for idx, line in enumerate(lines):
+            line = line.strip().split()
+            line = [float(x) for x in line]
+
             # bbox
-            annotations[idx, 0] = bbox[0]               # x1
-            annotations[idx, 1] = bbox[1]               # y1
-            annotations[idx, 2] = bbox[0] + bbox[2]     # x2
-            annotations[idx, 3] = bbox[1] + bbox[3]     # y2
+            annotations[idx, 0] = line[0]               # x1
+            annotations[idx, 1] = line[1]               # y1
+            annotations[idx, 2] = line[0] + line[2]     # x2
+            annotations[idx, 3] = line[1] + line[3]     # y2
 
             if self.is_train:
                 # landmarks
-                annotations[idx, 4] = bbox[4]           # l0_x
-                annotations[idx, 5] = bbox[5]           # l0_y
-                annotations[idx, 6] = bbox[7]           # l1_x
-                annotations[idx, 7] = bbox[8]           # l1_y
-                annotations[idx, 8] = bbox[10]          # l2_x
-                annotations[idx, 9] = bbox[11]          # l2_y
-                annotations[idx, 10] = bbox[13]         # l3_x
-                annotations[idx, 11] = bbox[14]         # l3_y
-                annotations[idx, 12] = bbox[16]         # l4_x
-                annotations[idx, 13] = bbox[17]         # l4_y
+                annotations[idx, 4] = line[4]               # l0_x
+                annotations[idx, 5] = line[5]               # l0_y
+                annotations[idx, 6] = line[7]               # l1_x
+                annotations[idx, 7] = line[8]               # l1_y
+                annotations[idx, 8] = line[10]              # l2_x
+                annotations[idx, 9] = line[11]              # l2_y
+                annotations[idx, 10] = line[13]             # l3_x
+                annotations[idx, 11] = line[14]             # l3_y
+                annotations[idx, 12] = line[16]             # l4_x
+                annotations[idx, 13] = line[17]             # l4_y
 
-                if annotations[idx, 4] < 0:
+                if (annotations[idx, 4]<0):
                     annotations[idx, 14] = -1
                 else:
                     annotations[idx, 14] = 1
+            
             else:
                 annotations[idx, 14] = 1
 
@@ -119,6 +99,7 @@ def log_dataset(use_artifact,
         artifact.add_file(artifact_path)
     run.log_artifact(artifact)
 
+
 def detection_collate(batch):
     """Custom collate fn for dealing with batches of images that have a different
     number of associated object annotations (bounding boxes).
@@ -133,7 +114,7 @@ def detection_collate(batch):
     imgs = []
 
     for _, (image, target) in enumerate(batch):
-        image = torch.from_numpy(image)
+        image  = torch.from_numpy(image)
         target = torch.from_numpy(target).to(dtype=torch.float)
 
         imgs.append(image)
